45d14e6f2d40491155f50729807e848aca6b22d9
vitaliy abrosimov <vitalikabrosimov00@gmail.com>
2026-04-23 12:21:58 +0300

    added discounts page

---
+++ src/scripts/discounts.js
@@ -0,0 +1,31 @@
+import {Swiper} from "swiper";
+import {Navigation} from "swiper/modules";
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.discounts__slider', {
+        modules: [Navigation],
+        slidesPerView: 1,
+        spaceBetween: 10,
+        simulateTouch: true,
+        allowTouchMove: true,
+
+        navigation: {
+            nextEl: '.discounts__btn.next',
+            prevEl: '.discounts__btn.prev',
+        },
+
+        breakpoints: {
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 15
+            },
+            1023: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            }
+        }
+    })
+})
