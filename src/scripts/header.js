9c7a4f4420d59bf55d12f24dd7b388fd584042c6
vitalik abrosimov <vitalikabrosimov00@gmail.com>
2026-09-01 16:44:28 +0300

    обновленный header-menu на пк

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,17 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const burgerMenu = document.querySelector('.burger-menu')
-    const header = document.querySelector('.header')
-
     const headerMenu = document.querySelector('.header-menu')
-    const headerMenuBtnOpen = document.querySelector('.header__link--has-submenu')
-
-    if (!header) return
-
-    const classes = {
-        isActive: 'is-active',
-        isOpen: 'is-open',
-        isLock: 'is-lock',
-    }
 
     const setTopToBurgerMenu = () => {
         const rect = headerMenu.getBoundingClientRect()
@@ -29,26 +17,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const debouncedSetTop = debounce(setTopToBurgerMenu, 250)
     window.addEventListener('resize', debouncedSetTop)
-
-    window.addEventListener('click', (e) => {
-        const headerMenuContent = headerMenu.querySelector('.header-menu__content')
-        const isClickInsideMenu = headerMenuContent.contains(e.target)
-        const isClickOnButton = headerMenuBtnOpen.contains(e.target)
-
-        if (!isClickInsideMenu && !isClickOnButton && headerMenu.classList.contains(classes.isActive)) {
-            headerMenuBtnOpen.click()
-        }
-    })
-
-    headerMenuBtnOpen.addEventListener('click', () => {
-        headerMenu.classList.toggle(classes.isActive)
-
-        if (headerMenu.classList.contains(classes.isActive)) {
-            headerMenuBtnOpen.parentElement.classList.add(classes.isOpen)
-            document.documentElement.classList.add(classes.isLock)
-        } else {
-            headerMenuBtnOpen.parentElement.classList.remove(classes.isOpen)
-            document.documentElement.classList.remove(classes.isLock)
-        }
-    })
 })
