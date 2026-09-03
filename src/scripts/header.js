03676fcaa2d860cec589f9b6d45128d2b7d24d10
vitalik abrosimov <vitalikabrosimov00@gmail.com>
2026-09-03 16:40:38 +0300

    правки меню

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,20 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
     const headerMenu = document.querySelector('.header-menu')
+    if (!headerMenu) return
 
-    const setTopToBurgerMenu = () => {
-        const rect = headerMenu.getBoundingClientRect()
-        headerMenu.style.setProperty('--top', `${rect.top / 16}rem`)
-    }
-    setTopToBurgerMenu()
-
-    const debounce = (func, delay) => {
-        let timeoutId
-        return (...args) => {
-            clearTimeout(timeoutId)
-            timeoutId = setTimeout(() => func.apply(this, args), delay)
-        }
-    }
-
-    const debouncedSetTop = debounce(setTopToBurgerMenu, 250)
-    window.addEventListener('resize', debouncedSetTop)
+    const rect = headerMenu.getBoundingClientRect()
+    headerMenu.style.setProperty('--top', `${rect.top / 16}rem`)
 })
